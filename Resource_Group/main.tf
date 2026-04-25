b93a456c8f6122c353bba28d81b4869a5f125d99
Asish Kashyap <129969015+asishkashyap@users.noreply.github.com>
2026-04-25 12:49:22 +0530

    Update main.tf

--- Resource_Group/main.tf
+++ Resource_Group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg_block" {
-  name       = "bhagwa-engg-2027"
+  name       = "bhagwa-engg-2026"
   location   = "Central India"
   managed_by = "Devops_Bhagwa"
   tags = {
